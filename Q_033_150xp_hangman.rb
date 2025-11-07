9b196c8f48ddde0b7c2939beb26216d7400fd88f
NullFluxKevin <null_flux_kevin@outlook.com>
2025-11-07 18:05:07 -0500

    hangman

---
+++ Q_033_150xp_hangman.rb
@@ -0,0 +1,79 @@
+
+class Hangman
+
+  def initialize(extra_guesses, words)
+    @target_word = words.sample.upcase
+    @remaining_guesses = @target_word.size + extra_guesses
+    @masked_word = "_" * @target_word.size
+    @wrong_letters = []
+  end
+
+  def valid?(letter)
+    return false if not /^[a-z]$/.match? letter
+
+    letter = letter.upcase
+
+    if @masked_word.include? letter or @wrong_letters.include? letter
+      return false
+    end
+
+    if @target_word.include?(letter)
+      @target_word.chars.each.with_index do |c, i|
+        @masked_word[i] = letter if c == letter
+      end
+    else
+      @wrong_letters.push(letter)
+    end
+
+    @remaining_guesses -= 1
+
+    true
+  end
+
+  def state(with_prompt = true)
+    parts = [
+      "Word:",
+      @masked_word,
+      "Remaining: #{@remaining_guesses}",
+      "Incorrect: #{@wrong_letters.join}",
+    ]
+
+    parts << "Guess: " if with_prompt
+
+    parts.join(" | ")
+  end
+
+
+  def over?
+    not @masked_word.include? "_" or @remaining_guesses == 0
+  end
+
+
+  def start
+    until over?
+      loop do
+        print state
+        letter = gets.chomp
+        break if valid? letter
+      end
+    end
+
+    puts state(false)
+    if @masked_word.include? "_"
+      puts "You LOSE!"
+    else
+      puts "You WIN!"
+    end
+  end
+
+end
+
+
+if __FILE__ == $0
+  words = %w{ immutable game hangman }
+
+  extra_guesses = 3
+  game = Hangman.new(extra_guesses, words)
+
+  game.start
+end
